0a01c025299f8086824579c2401f992f0e80fce2
luiz g <luizgcarvalhob@gmail.com>
2026-06-01 00:03:16 -0300

    >Correção de parametros no arquivo cleaner.py e criação da função _calcular_faixa_etaria >Ajustes nas funções do arquivo filters.py

--- src/layers/data/cleaner.py
+++ src/layers/data/cleaner.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import numpy as np
 
-RA_VALIDAS = {
-    "ÁGUAS CLARAS", "ASA NORTE", "ASA SUL", "BRAZLÂNDIA", "CANDANGOLÂNDIA", 
-    "CEILÂNDIA", "CRUZEIRO", "FERCAL", "GAMA", "GUARÁ", "LAGO NORTE", 
-    "LAGO SUL", "NÚCLEO BANDEIRANTE", "PARANOÁ", "PARK WAY", "PLANALTINA", 
-    "RECANTO DAS EMAS", "RIACHO FUNDO", "SAMAMBAIA", "SANTA MARIA", 
-    "SÃO SEBASTIÃO", "SCIA", "SIA", "SOBRADINHO", "SUDOESTE/OCTOGONAL", 
-    "TAGUATINGA", "VARJÃO", "VICENTE PIRES"
+REGIAO_VALIDAS = {
+    "6642": "Central (Plano Piloto/Lagoa/Sudoeste)",
+    "6643": "Centro-Sul (Guará/Bandeirante/Riacho)",
+    "6644": "Norte (Planaltina/Sobradinho)",
+    "6645": "Sudoeste (Taguatinga/Samambaia/Aguas Claras)",
+    "6646": "Oeste (Ceilândia/Brazlândia)",
+    "6647": "Sul (Gama/Santa Maria)",
+    "6648": "Leste (Paranoá/São Sebastião/Itapoã)"
 }
 
 SEXOS_VALIDOS = {"M", "F", "I"}
@@ -24,8 +26,8 @@ def limpar(df: pd.DataFrame) -> pd.DataFrame:
     df = _tratar_datas(df)
     df = _normalizar_strings(df)
     df = _remover_campos_vazios(df)
-    df = _validar_num_casos(df)
     df = _validar_dados(df)
+    df = _calcular_faixa_etaria(df)
     
     # Reseta o index do DataFrame para integers sequenciais, prevenindo index antigos de virarem uma nova coluna
     # Retorna o DataFrame limpo
@@ -37,47 +39,56 @@ def _remover_duplicatas(df: pd.DataFrame) -> pd.DataFrame:
     return df.drop_duplicates() # Retorna o DataFrame sem linhas duplicadas
 
 def _tratar_datas(df: pd.DataFrame) -> pd.DataFrame:
-    df["data_notificacao"] = pd.to_datetime(
-        df["data_notificacao"], errors="coerce"
-    ) # Converte os valores de data_notificacao para objetos datetime do Pandas. Dados inválidos serão convertidos para NaT
+    df["DT_NOTIFIC"] = pd.to_datetime(
+        df["DT_NOTIFIC"], errors="coerce"
+    ) # Converte os valores de DT_NOTIFIC para objetos datetime do Pandas. Dados inválidos serão convertidos para NaT
     
-    invalidas = df["data_notificacao"].isna().sum() # Conta a quantidade total de valores nulos na coluna data_notificacao
+    invalidas = df["DT_NOTIFIC"].isna().sum() # Conta a quantidade total de valores nulos na coluna data_notificacao
     if invalidas:
         print(f"[cleaner] {invalidas} linha(s) removida(s) por data inválida.") # Imprime a quantidade de linhas inválidas removidas caso existam
-    return df.dropna(subset="data_notificacao") # Retorna o DataFrame limpo de data_notificacao inválida
+    return df.dropna(subset="DT_NOTIFIC") # Retorna o DataFrame limpo de data_notificacao inválida
     
 def _normalizar_strings(df: pd.DataFrame) -> pd.DataFrame:
-    # Converte a coluna regiao_administrativa, remove espaços e converte todas as letras para maiúsculas
-    df["regiao_administrativa"] = df["regiao_administrativa"].astype(str).str.strip().str.upper()
-    df["sexo"] = df["sexo"].astype(str).str.strip().str.upper() # Converte a coluna sexo para string, remove espaços e converte todas as letras para maiúsculas
-    df["faixa_etaria"] = df["faixa_etaria"].astype(str).str.strip() # Converte a coluna faixa_etaria para string e remove espaços
+    # Converte a coluna ID_REGIONA, remove espaços e converte todas as letras para maiúsculas
+    df["ID_REGIONA"] = df["ID_REGIONA"].astype(str).str.split('.').str.strip().str.upper()
+    df["CS_SEXO"] = df["CS_SEXO"].astype(str).str.strip().str.upper() # Converte a coluna CS_SEXO para string, remove espaços e converte todas as letras para maiúsculas
+    df["NU_IDADE_N"] = df["NU_IDADE_N"].astype(str).str.strip() # Converte a coluna NU_IDADE_N para string e remove espaços
     return df # Retorna o DataFrame com strings normalizadas
 
 def _remover_campos_vazios(df: pd.DataFrame) -> pd.DataFrame:
     antes = len(df) # Atribui à variável "antes" a quantidade total de linhas do DataFrame
-    df = df.dropna(subset=["regiao_administrativa", "num_casos", "sexo", "faixa_etaria"]) # Remove os campos nulos das colunas do DataFrame
+    df = df.dropna(subset=["ID_REGIONA", "CS_SEXO", "NU_IDADE_N"]) # Remove os campos nulos das colunas do DataFrame
     removidas = antes - len(df) # Subtrai a quantidade anterior de linhas pelo DataFrame limpo para encontrar a quantidade total de itens removidos
     if removidas:
         print(f"[cleaner] {removidas} linha(s) removida(s) por campos vazios.") # Imprime a quantidade de linhas removidas por terem campos nulos
     return df # Retorna o DataFrame sem campos nulos
 
-def _validar_num_casos(df: pd.DataFrame) -> pd.DataFrame:
-    df["num_casos"] = pd.to_numeric(df["num_casos"], errors="coerce") # Converte os valores de num_casos para um dado numérico. Em caso do valor que não puder ser convertido para número, converte para NaN
-    invalidos = df["num_casos"].isna() | (df["num_casos"] < 0) # Atribui à variável invalidos todos os valores que forem nulos ou menores que zero
-    if invalidos.sum():
-        print(f"[cleaner] {invalidos.sum()} linha(s) removida(s) de num_casos inválidos.") # Imprime a quantidade total de valores inválidos encontrados na coluna num_casos
-    df_valido = df[~invalidos].copy() # Remove todos os NaN do DataFrame. O método .copy() garante que a alteração ocorra em um DataFrame independente, ao invés de uma fatia do original
-    df_valido["num_casos"] = df_valido["num_casos"].astype(int) # Converte a coluna num_casos do DataFrame para int
-    return df_valido
+def _calcular_faixa_etaria(df: pd.DataFrame) -> pd.DataFrame:
+    # Converte para int temporariamente para fazer a matemática do DATASUS
+    idades_brutas = pd.to_numeric(df["NU_IDADE_N"], errors='coerce').fillna(0).astype(int)
+    
+    # Considerando apenas dados possiveis de idade, de 0 anos de idade a 120
+    idade_anos = np.where((idades_brutas >= 4000) & (idades_brutas <= 4120), idades_brutas - 4000, 0)
+    
+    # Agrupa nas faixas definidas em FE_VALIDAS
+    df["FX_ETARIA"] = pd.cut(
+        idade_anos,
+        bins=[0, 10, 20, 40, 60, 80, np.inf],
+        labels=["0-9", "10-19", "20-39", "40-59", "60-79", "80+"],
+        right=False
+    ).astype(str)
+    
+    return df
+
 
 def _validar_dados(df: pd.DataFrame) -> pd.DataFrame:
-    ra = df["regiao_administrativa"].isin(RA_VALIDAS) # Verifica se os valores da coluna regiao_administrativa estão no set RA_VALIDAS
-    sexo = df["sexo"].isin(SEXOS_VALIDOS) # Verifica se os valores da coluna sexo estão no set SEXOS_VALIDOS
-    fe = df["faixa_etaria"].isin(FE_VALIDAS) # Verifica se os valores da coluna faixa_etaria estão no set FE_VALIDAS
+    ra = df["ID_REGIONA"].isin(REGIAO_VALIDAS) # Verifica se os valores da coluna ID_REGIONA estão no set RA_VALIDAS
+    sexo = df["CS_SEXO"].isin(SEXOS_VALIDOS) # Verifica se os valores da coluna sexo estão no set SEXOS_VALIDOS
+    fe = df["NU_IDADE_N"].isin(FE_VALIDAS) # Verifica se os valores da coluna faixa_etaria estão no set FE_VALIDAS
     
     # Caso algum valor desconhecido seja encontrado, imprime:
     if(~ra).sum():
-        print(f"[cleaner]: {(~ra).sum()} linha(s) com RA desconhecida(s): {df.loc[~ra, "regiao_administrativa"].unique()}") # A quantidade de valores desconhecidos na coluna regiao_administrativa
+        print(f"[cleaner]: {(~ra).sum()} linha(s) com RA desconhecida(s): {df.loc[~ra, "ID_REGIONA"].unique()}") # A quantidade de valores desconhecidos na coluna ID_REGIONA
     if(~sexo).sum():
         print(f"[cleaner]: {(~sexo).sum()} linha(s) com sexo inválido.") # A quantidade de valores desconhecidos na coluna sexo
     if(~fe).sum():
--- src/layers/data/filters.py
+++ src/layers/data/filters.py
@@ -2,14 +2,13 @@ import pandas as pd
 from src.layers.data.cleaner import limpar
 from src.layers.data.loader import data_loadSinan
 
-def _filter(ano: int, mes: str=None,uf: str=None, xfilters: dict= None)->int: #função filtro base/principal para as outras
+def _filter(ano: int, mes: str=None,uf: str=None, xfilters: dict= None, return_df: bool= False): #função filtro base/principal para as outras
 
     df = limpar(data_loadSinan(ano))
     condition = pd.Series(True, index=df.index)
 
     if mes: #verifica se existe busca por mes
-        date_search = f"{ano}{mes}"
-        condition &= df["DT_NOTIFIC"].astype(str).str.contains(date_search, na=False)
+        condition &= (df["DT_NOTIFIC"].dt.month == mes)
     
     if uf: #verifica se existe busca pela UF
         condition &= (df["SG_UF_NOT"] == uf)
@@ -19,43 +18,35 @@ def _filter(ano: int, mes: str=None,uf: str=None, xfilters: dict= None)->int: #f
             if column in df.columns:
                 condition = condition & (df[column] == value)
     
-    return df[condition].shape
+    df_filtrado = df[condition]
+
+    if return_df:
+        return df_filtrado
+    
+    return df_filtrado.shape[0]
 
 def filter_distrib_idade(ano: int, mes: str=None, uf: str=None): #função para filtrar a distribuição de idades
-    df = limpar(data_loadSinan(ano))
-    condition = pd.Series(True, index=df.index)
+    dfFiltrado = _filter(ano, mes=mes, uf=uf, return_df=True)
 
-    if mes:
-        date_search = f"{ano}{mes}"
-        condition &= df["DT_NOTIFIC"].astype(str).str.contains(date_search, na=False)
-    
-    if uf:
-        condition &= (df["SG_UF_NOT"] == uf)
-
-    dfFiltrado = df[condition].copy()
-
-    dfFiltrado = dfFiltrado[(dfFiltrado["NU_IDADE_N"] >= 4000) & dfFiltrado["NU_IDADE_N"] <= 4120]
-    dfFiltrado["IDADE_LIMPA"] = dfFiltrado["NU_IDADE_N"] - 4000
-
-    counter_idade = dfFiltrado["IDADE_LIMPA"].value_counts().sort_index()
+    counter_idade = dfFiltrado["FX_ETARIA"].value_counts().sort_index()
 
     return counter_idade
 
 def filter_df(ano: int, mes: str=None, uf: str="53"): #função derivada da principal para filtrar somente por UF
-    total = _filter(ano, mes, uf)
+    total = _filter(ano, mes=mes, uf=uf)
     return total
 
 def filter_sexo_df(ano: int, sex: str, mes: str=None, uf: str="53"): #função derivada da principal para filtrar sexo na UF
     filtro = {"CS_SEXO": sex}
-    total = _filter(ano, mes, uf, filtro)
+    total = _filter(ano, mes=mes, uf=uf, xfilters=filtro)
     return total
 
 def filter_sexo(ano: int, sex: str, mes: str=None): #função derivada da principal para filtrar somente por sexo
     filtro = {"CS_SEXO": sex}
-    total = _filter(ano, mes, filtro)
+    total = _filter(ano, mes=mes, uf=None, xfilters=filtro)
     return total
 
-def filter_idade_df(ano: int, idade: int, mes: str=None, uf: str="53"): #função derivada da principal para filtrar idade especifica na UF
-    filtro = {"NU_IDADE_N": idade + 4000}
-    total = _filter(ano, mes, uf, filtro)
+def filter_idade_df(ano: int, fx_etaria: str, mes: str=None, uf: str="53"): #função derivada da principal para filtrar idade especifica na UF
+    filtro = {"FX_ETARIA": fx_etaria}
+    total = _filter(ano, mes=mes, uf=uf, xfilters=filtro)
     return total
